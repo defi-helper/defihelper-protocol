91be631aa93be196d797d3a10e61002860459284
Vladislav Komissarov <mastyf@gmail.com>
2021-10-24 21:28:06 +0300

    Error: expected 1 constructor arguments, got 2

--- deploy/main/005_balance/001_Balance.js
+++ deploy/main/005_balance/001_Balance.js
@@ -2,10 +2,9 @@ const { migration } = require('../../utils');
 
 module.exports = migration(async ({ getNamedAccounts, utils: { get, deploy } }) => {
   const treasury = await get('Treasury');
-  const { inspector } = await getNamedAccounts();
 
   await deploy('Balance', {
-    args: [treasury.address, inspector],
+    args: [treasury.address],
   });
 });
 module.exports.tags = ['Protocol'];
