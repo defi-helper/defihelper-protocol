c901430e52e7d510f49b17b9d461c821240b973e
artur <amamedbekov@tokenomica.com>
2022-02-11 14:25:17 +0300

    fix

--- deploy/lite/008_test/001_Storage.js
+++
@@ -1,6 +0,0 @@
-const { migration } = require('../../utils');
-
-module.exports = migration(async ({ getNamedAccounts, utils: { get, execute, deploy } }) => {
-  await deploy('Storage', {});
-});
-module.exports.tags = ['Protocol'];
--- deploy/lite/008_test/002_Treasury.js
+++
@@ -1,6 +0,0 @@
-const { migration } = require('../../utils');
-
-module.exports = migration(async ({ utils: { deploy} }) => {
-  await deploy('Treasury', {});
-});
-module.exports.tags = ['Protocol'];
