abf9793e42a22a48051790e55d85970fd714e110
artur <amamedbekov@tokenomica.com>
2022-06-17 17:13:57 +0300

    fix deploy delegator

--- deploy/main/012_delegator/002_TimelockOwner.js
+++ deploy/main/012_delegator/002_TimelockOwner.js
@@ -2,10 +2,10 @@ const { migration } = require('../../utils');
 
 module.exports = migration(async ({ utils: { read, get, execute } }) => {
   const timelock = await get('Timelock');
-  if ((await read('BuyLiquidity', {}, 'owner')) === timelock.address) {
+  if ((await read('Delegator', {}, 'owner')) === timelock.address) {
     return;
   }
 
-  await execute('BuyLiquidity', {}, 'transferOwnership', timelock.address);
+  await execute('Delegator', {}, 'transferOwnership', timelock.address);
 });
 module.exports.tags = ['GovernanceOwner'];
